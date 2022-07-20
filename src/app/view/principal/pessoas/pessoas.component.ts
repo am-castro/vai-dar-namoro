60bb3ea0ddec516f076756bb5a23ddd26ea46c02
Antonio Marcos <antoniomarcos.mc36@gmail.com>
2022-07-20 04:48:32 -0300

    Chat em funcionamento
    Resta integrar a API

--- src/app/view/principal/pessoas/pessoas.component.ts
+++ src/app/view/principal/pessoas/pessoas.component.ts
@@ -1,16 +1,18 @@
 import { UserService } from './../../../component/services/user/user.service';
 import { Component, OnInit } from '@angular/core';
-import { faPlus, faGear, faBars, faCommentDots, faComments } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faGear, faBars, faCommentDots, faComments, faPaperPlane, faFaceKissBeam } from '@fortawesome/free-solid-svg-icons';
 import { PessoasService } from 'src/app/component/services/pessoas/pessoas.service';
 
 @Component({
   selector: 'app-pessoas',
   templateUrl: './pessoas.component.html',
-  styleUrls: ['./pessoas.component.css']
+  styleUrls: ['./pessoas.component.scss']
 })
 export class PessoasComponent implements OnInit {
-  public icons = {config: faGear, new: faPlus, menu: faBars, haveMessage: faCommentDots, messages: faComments}
+  public icons = {config: faGear, new: faPlus, menu: faBars, haveMessage: faCommentDots, messages: faComments, send: faPaperPlane, icon: faFaceKissBeam}
   public pessoa: any;
+
+  public msgs: any = [];
 
   public mobileQuery?: MediaQueryList;
 
@@ -26,5 +28,9 @@ export class PessoasComponent implements OnInit {
     }
 
   ngOnInit() {
+  }
+
+  onSend(texto: string){
+    this.msgs.push({text: texto});
   }
 }
